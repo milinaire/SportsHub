79f64ff7c90007bde684a58422501c25b80c5e33
Unfectum <roma509000@gmail.com>
2021-03-25 18:43:40 +0200

    for local

--- SportsHubWEB/ClientApp/src/components/NavMenu.js
+++ SportsHubWEB/ClientApp/src/components/NavMenu.js
@@ -1,19 +1,79 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import './Styles.css';
 
 export class NavMenu extends Component {
+    state = {
+        Categories: [
+            {
+                title: "Category1"
+            },
+            {
+                title: "Category2"
+            },
+            {
+                title: "Category3"
+            },
+            {
+                title: "Category1"
+            },
+            {
+                title: "Category1"
+            },
+            {
+                title: "Category2"
+            },
+            {
+                title: "Category3"
+            },
+            {
+                title: "Category1"
+            },
+            {
+                title: "Category2"
+            },
+            {
+                title: "Category2"
+            },
+            {
+                title: "Category3"
+            }
+        ],
+        follow: [
+            {
+                title: "Category1"
+            },
+            {
+                title: "Category2"
+            },
+            {
+                title: "Category3"
+            },
+            {
+                title: "Category1"
+            },
 
-  render () {
-    return (
+        ]
+    }
 
-          <div className={"left-panel-wrapper"}>
-              <div className="menu">
-                  some content some content some content some content some content some content some content some
-                  content some content
-              </div>
-          </div>
+    render() {
+        return (
 
-    );
-  }
+            <div className={"left-panel-wrapper"}>
+                <div className="menu">
+                    <div className={"categories"}>
+                        <h3>Home</h3>
+                        {this.state.Categories.map(Category => (
+                            <h3>{Category.title}</h3>
+                        ))}
+                        <h3>TEAM HUB</h3>
+                        <h3>LIFESTYLE</h3>
+                        <h3>DEALBOOK</h3>
+                        <h3>VIDEO</h3>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
 }

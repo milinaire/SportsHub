e725f5a827f0ce141b6b979894b6966a2a98c7a3
DudaOleksandr <olexandrduda@gmail.com>
2021-03-25 19:50:49 +0200

    Article base(rout + page)

--- SportsHubWEB/ClientApp/src/components/Main/Home.js
+++ SportsHubWEB/ClientApp/src/components/Main/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -11,7 +13,8 @@ export class Home extends Component {
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+          <li><a href='/article'>Bootstrap</a> for layout and styling</li>
+            
         </ul>
         <p>To help you get started, we have also set up:</p>
         <ul>

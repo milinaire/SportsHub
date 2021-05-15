3f42e5a6056d44af4aeb7a476bf46cc6bdf91f0e
DudaOleksandr <olexandrduda@gmail.com>
2021-05-15 23:59:22 +0300

    Update for ImageService added

--- SportsHubBL/Interfaces/IImageService.cs
+++ SportsHubBL/Interfaces/IImageService.cs
@@ -12,14 +12,14 @@ namespace SportsHubBL.Interfaces
     public interface IImageService
     {
         public Image GetImageById(int id);
-        
-        public Image UpdateImageById(int id, string uri);
-        
+
         public void DeleteTeamById(int id);
         
         public Image GetImage(string uri);
         
         public ImageModel GetModel(Image image);
+        
+        public ImageModel UpdateImageById(int id, string uri);
         
         public Task AddImage(IFormFile imageFile);
         
